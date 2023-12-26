d06cf236fc0f3688dbdd2a9fee814a0b58db7e81
Abdul Elah JS <abdul.elah.js@gmail.com>
2023-12-26 14:33:52 +0300

    disabled s command keymappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,14 +5,14 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.del({ "n", "v" }, "<leader>x")
-
 keymap.set("i", "kj", "<Escape>", opts)
 keymap.set("i", "jk", "<Escape>", opts)
-keymap.set("v", "s", "c", opts)
-keymap.set("n", "s", "cl", opts)
+-- keymap.set("v", "s", "c", opts)
+-- keymap.set("n", "s", "cl", opts)
 keymap.set({ "v", "n" }, "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
 keymap.set({ "v", "n" }, "<Tab>", ":BufferLineCycleNext<CR>", opts)
 keymap.set({ "v", "n" }, "<leader>x", ":bd<CR>", opts)
 keymap.set({ "n", "v" }, ">", ">>", opts)
 keymap.set({ "n", "v" }, "<", "<<", opts)
+-- keymap.set({ "n", "v" }, "<leader>th", ":ToggleTerm direction=horizontal", opts)
+-- keymap.set({ "n", "v" }, "<leader>tf", ":ToggleTerm direction=float", opts)
