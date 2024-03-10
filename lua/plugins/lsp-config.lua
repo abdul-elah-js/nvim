281a3ee4c67c7035b2d01c6ecbd3d9ebdaaaef62
Abdulelah <abdul.elah.js@gmail.com>
2024-03-10 13:10:35 +0300

    added symlink to .vimrc, keymap for Redo

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "typescript",
+            "javascript",
+            "markdown",
+            "ejs",
+          },
+          init_options = {
+            html = {
+              options = {
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
