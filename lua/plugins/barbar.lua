86fe0a6ea7bf42e7deb24a5da9fc9448fbdb4762
Abdulelah <abdul.elah.js@gmail.com>
2024-05-25 08:18:24 +0300

    tokyo-night transparent

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,17 +1,4 @@
 return {
   "romgrk/barbar.nvim",
-  dependencies = {
-    "lewis6991/gitsigns.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  init = function()
-    vim.g.barbar_auto_setup = false
-  end,
-  opts = {
-    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
-    -- …etc.
-  },
-  version = "^1.0.0",
+  disabled = true,
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "neofusion",
+    },
+  },
+
+  -- {
+  --   "diegoulloao/neofusion.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("neofusion").setup({
+  --       transparent_mode = true,
+  --     })
+  --   end,
+  -- },
 
   {
     "rcarriga/nvim-notify",
@@ -15,22 +36,3 @@ return {
     },
   },
 }
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "tokyonight",
---     },
---   },
--- }
-
--- return {
---   { "catppuccin/nvim" },
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "catppuccin",
---     },
---   },
--- }
